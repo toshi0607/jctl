d2ab8f81399e084b57fea8b43d237622634f8e37
toshi0607 <s.toshi0607@gmail.com>
2019-09-29 23:14:51 +0900

    :recycle: decouple K8s

--- pkg/cli/cli.go
+++ pkg/cli/cli.go
@@ -1,22 +1,14 @@
 package cli
 
 import (
-	"flag"
 	"fmt"
 	"io"
 	"log"
-	"os"
-	"path/filepath"
 
 	"github.com/jessevdk/go-flags"
 	"github.com/pkg/errors"
 	"github.com/toshi0607/jctl/pkg/gobuild"
-	batchv1 "k8s.io/api/batch/v1"
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
-	"k8s.io/client-go/tools/clientcmd"
+	"github.com/toshi0607/jctl/pkg/kubernetes"
 )
 
 type CLI interface {
@@ -31,10 +23,11 @@ type (
 	}
 
 	config struct {
-		Namespace string `short:"s" long:"namespace" description:""`
-		Version   bool   `short:"v" long:"version" description:"Show version"`
-		Help      bool   `short:"h" long:"help" description:"Show this help message"`
-		Args      struct {
+		Namespace  string `short:"s" long:"namespace" default:"default" description:""`
+		Version    bool   `short:"v" long:"version" description:"Show version"`
+		Help       bool   `short:"h" long:"help" description:"Show this help message"`
+		KubeConfig string `long:"kubeconfig" description:"absolute path to K8s credential"`
+		Args       struct {
 			Path string
 		} `positional-args:"yes"`
 	}
@@ -89,84 +82,15 @@ func (c *cli) Run() int {
 	}
 	fmt.Println("published")
 
-	var kubeconfig *string
-	if home := homeDir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "kind-config-kind"), "(optional) absolute path to the kubeconfig file")
-	} else {
-		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-	}
-	flag.Parse()
-	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	k, err := kubernetes.New(c.OutStream, c.ErrStream, c.Config.Namespace, c.Config.KubeConfig)
 	if err != nil {
 		log.Fatal(err)
 	}
-	clientset, err := kubernetes.NewForConfig(config)
+
+	err = k.Create(ref.Name())
 	if err != nil {
 		log.Fatal(err)
 	}
-	// construct job
-	// TODO: namespaceはフラグ指定
-	jobName := "jctl-job"
-	job := &batchv1.Job{
-		TypeMeta: metav1.TypeMeta{APIVersion: batchv1.SchemeGroupVersion.String(), Kind: "Job"},
-		ObjectMeta: metav1.ObjectMeta{
-			GenerateName: jobName,
-			Namespace:    "default",
-		},
-		Spec: batchv1.JobSpec{
-			Template: corev1.PodTemplateSpec{
-				Spec: corev1.PodSpec{
-					Containers: []corev1.Container{
-						{
-							Name:  jobName,
-							Image: ref.Name(),
-						},
-					},
-					ImagePullSecrets: []corev1.LocalObjectReference{{Name: "image-puller"}},
-					RestartPolicy:    corev1.RestartPolicyNever,
-				},
-			},
-		},
-	}
-	createdJob, err := clientset.BatchV1().Jobs("default").Create(job)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(createdJob)
 
-	w, err := clientset.BatchV1().Jobs("default").Watch(metav1.ListOptions{})
-	defer w.Stop()
-	ch := w.ResultChan()
-	for {
-		select {
-		case obj := <-ch:
-			job, ok := obj.Object.(*batchv1.Job)
-			if !ok {
-				fmt.Printf("who are you? %v", obj)
-			}
-			if createdJob.Name == job.Name &&
-				IsJobFinished(job) {
-				fmt.Printf("Job: %s finished", createdJob.Name)
-				return 0
-			}
-		}
-	}
-	return 1
-}
-
-func IsJobFinished(j *batchv1.Job) bool {
-	for _, c := range j.Status.Conditions {
-		if (c.Type == batchv1.JobComplete || c.Type == batchv1.JobFailed) && c.Status == corev1.ConditionTrue {
-			return true
-		}
-	}
-	return false
-}
-
-// TODO: check KUBECONFIG as well
-func homeDir() string {
-	if h := os.Getenv("HOME"); h != "" {
-		return h
-	}
-	return os.Getenv("USERPROFILE") // windows
+	return 0
 }
--- pkg/kubernetes/kubernetes.go
+++ pkg/kubernetes/kubernetes.go
@@ -1,1 +1,136 @@
 package kubernetes
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+
+	batchv1 "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+type JobCli interface {
+	Create(image string) error
+}
+
+type (
+	jobCli struct {
+		OutStream, ErrStream io.Writer
+		Clientset            *kubernetes.Clientset
+		Namespace            string
+	}
+)
+
+func New(outStream, errStream io.Writer, ns, kc string) (JobCli, error) {
+	kubeConfig, err := getKubeConfig(kc)
+	if err != nil {
+		return nil, err
+	}
+	config, err := clientcmd.BuildConfigFromFlags("", kubeConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &jobCli{
+		OutStream: outStream,
+		ErrStream: errStream,
+		Namespace: ns,
+		Clientset: clientset,
+	}, nil
+}
+
+func getKubeConfig(kc string) (string, error) {
+	if kc != "" {
+		return kc, nil
+	}
+	if os.Getenv("KUBECONFIG") != "" {
+		return os.Getenv("KUBECONFIG"), nil
+	}
+	if defaultConfig() != "" {
+		return defaultConfig(), nil
+	}
+	return "", errors.New("kubectx not found")
+}
+
+func defaultConfig() string {
+	if home := homeDir(); home != "" {
+		return filepath.Join(home, ".kube", "config")
+	}
+	return ""
+}
+
+func IsJobFinished(j *batchv1.Job) bool {
+	for _, c := range j.Status.Conditions {
+		if (c.Type == batchv1.JobComplete || c.Type == batchv1.JobFailed) && c.Status == corev1.ConditionTrue {
+			return true
+		}
+	}
+	return false
+}
+
+func homeDir() string {
+	if h := os.Getenv("HOME"); h != "" {
+		return h
+	}
+	// windows
+	return os.Getenv("USERPROFILE")
+}
+
+func (c *jobCli) Create(image string) error {
+	jobName := "jctl-job"
+	job := &batchv1.Job{
+		TypeMeta: metav1.TypeMeta{APIVersion: batchv1.SchemeGroupVersion.String(), Kind: "Job"},
+		ObjectMeta: metav1.ObjectMeta{
+			GenerateName: jobName,
+			Namespace:    c.Namespace,
+		},
+		Spec: batchv1.JobSpec{
+			Template: corev1.PodTemplateSpec{
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  jobName,
+							Image: image,
+						},
+					},
+					ImagePullSecrets: []corev1.LocalObjectReference{{Name: "image-puller"}},
+					RestartPolicy:    corev1.RestartPolicyNever,
+				},
+			},
+		},
+	}
+	createdJob, err := c.Clientset.BatchV1().Jobs(c.Namespace).Create(job)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(createdJob)
+
+	w, err := c.Clientset.BatchV1().Jobs(c.Namespace).Watch(metav1.ListOptions{})
+	defer w.Stop()
+	ch := w.ResultChan()
+	for {
+		select {
+		case obj := <-ch:
+			job, ok := obj.Object.(*batchv1.Job)
+			if !ok {
+				fmt.Printf("who are you? %v", obj)
+			}
+			if createdJob.Name == job.Name &&
+				IsJobFinished(job) {
+				fmt.Printf("Job: %s finished", createdJob.Name)
+				return nil
+			}
+		}
+	}
+}
